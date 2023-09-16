4e938579f7b21617886172d60797478c85a77b2f
JulioCesarAlves <calves8julio@gmail.com>
2023-09-16 01:06:31 -0300

    feat: adiciona layout para tela de login

--- src/components/organisms/LoginForm/page.tsx
+++ src/components/organisms/LoginForm/page.tsx
@@ -1,14 +1,33 @@
 "use client";
-import { signIn } from "next-auth/react";
+import CadastrarButton from "@components/atoms/CadastrarButton/page";
+import LoginButton from "@components/atoms/LoginButton/page";
+import Image from "next/image";
 import React from "react";
 
 const LoginForm = () => {
   return (
-    <div className="flex flex-col">
-      LoginForm
-      <button className="bg-white" onClick={() => signIn("keycloak")}>
-        Sign in
-      </button>
+    <div className="flex flex-col items-center gap-12">
+      <Image
+        alt="rinha title"
+        src={"/images/rinha-title.svg"}
+        width={450}
+        height={87}
+      />
+      <div className="flex flex-col items-center gap-20">
+        <div className="w-[28.125rem]">
+          <p className="font-sans font-bold text-white text-lg">
+            Bem vindo à rinha de backend!
+          </p>
+          <p className="font-sans text-white text-lg">
+            Faça o login abaixo para continuar ou se
+            cadastre para poder participar da competição.
+          </p>
+        </div>
+        <div className="flex flex-col gap-4">
+          <LoginButton />
+          <CadastrarButton />
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/templates/LoginTemplate/layout.tsx
+++ src/components/templates/LoginTemplate/layout.tsx
@@ -1,4 +1,5 @@
 "use client";
+import LoginLogo from "@components/atoms/LoginLogo/page";
 import Image from "next/image";
 import React from "react";
 
@@ -8,13 +9,16 @@ type Props = {
 
 const LoginTemplate = ({ form }: Props) => {
   return (
-    <div className="w-full">
+    <div className="w-full z-0">
       <Image
         alt="Login wallpaper"
         src="/images/login-wallpaper.jpg"
         layout="fill"
         className="object-cover"
       />
+      <div className="absolute z-10 top-12 left-12">
+        <LoginLogo />
+      </div>
       <div className="flex items-center justify-center absolute h-full w-2/6 right-0 bg-[#061127] opacity-90">
         {form}
       </div>
