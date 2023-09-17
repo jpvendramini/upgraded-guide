99c4ba6baa75234035ace6bad17bf9948c4be4be
João Pedro Vendramini <joao.vendramini@muralis.com.br>
2023-09-17 16:54:08 -0300

    fix: double import

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,4 @@
 import RankLayout from "@/components/templates/RankTemplate/layout";
-import Logo from "@components/atoms/Logo/page";
-import Profile from "@components/molecules/Profile/page";
 import Estatisticas from "@components/organisms/Estatisticas/page";
 import Podium from "@components/organisms/Podium/page";
 import Submissoes from "@components/organisms/Submissoes/page";
