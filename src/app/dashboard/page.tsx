45d13c09b5ba2922a0cd68d5238dceb8564edcf2
João Pedro Vendramini <joao.vendramini@muralis.com.br>
2023-09-17 12:10:12 -0300

    feat: dropzone

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,17 +6,20 @@ import Podium from "@components/organisms/Podium/page";
 import Submissoes from "@components/organisms/Submissoes/page";
 import Profile from "@components/molecules/Profile/page";
 import Logo from "@components/atoms/Logo/page";
+import DropZoneProvider from "@components/contexts/DropZoneContext";
 
 const Dashboard = () => {
   return (
-    <RankLayout
-      estatisticas={<Estatisticas />}
-      podium={<Podium />}
-      rank={<Rank />}
-      submissoes={<Submissoes />}
-      profile={<Profile />}
-      logo={<Logo />}
-    />
+    <DropZoneProvider>
+      <RankLayout
+        estatisticas={<Estatisticas />}
+        podium={<Podium />}
+        rank={<Rank />}
+        submissoes={<Submissoes />}
+        profile={<Profile />}
+        logo={<Logo />}
+      />
+    </DropZoneProvider>
   );
 };
 
--- src/components/atoms/LineChart/page.tsx
+++ src/components/atoms/LineChart/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useMemo } from "react"
+import { useMemo } from "react";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -8,8 +8,8 @@ import {
   LineElement,
   Tooltip,
   Filler,
-} from 'chart.js';
-import { Line } from 'react-chartjs-2';
+} from "chart.js";
+import { Line } from "react-chartjs-2";
 
 ChartJS.register(
   CategoryScale,
@@ -17,7 +17,7 @@ ChartJS.register(
   PointElement,
   LineElement,
   Tooltip,
-  Filler,
+  Filler
 );
 
 type Props = {
@@ -36,18 +36,16 @@ const LineChart = async ({ linePoints }: Props) => {
       datasets: [
         {
           fill: true,
-          label: '',
+          label: "",
           data: linePoints,
-          borderColor: '#FF7A00',
-          backgroundColor: '#FF7A004D',
+          borderColor: "#FF7A00",
+          backgroundColor: "#FF7A004D",
         },
       ],
-    }
+    };
     return data;
-  }, [])
-  return (
-    <Line options={options} data={lineData} />
-  );
+  }, []);
+  return <Line options={options} data={lineData} />;
 };
 
 export default LineChart;
