f668118bd892adc55d4db59f0050170ec6008df4
João Pedro Vendramini <joao.vendramini@muralis.com.br>
2023-09-20 19:38:27 -0300

    fix: comment api

--- src/hooks/useFetchPesoPesadoRanking.tsx
+++ src/hooks/useFetchPesoPesadoRanking.tsx
@@ -1,7 +1,7 @@
 import { useRankingContext } from "@contexts/RankingContext";
-import { useApi } from "@server/api";
+// import { useApi } from "@server/api";
 import { useQuery } from "react-query";
-import { RankingResultType } from "./types";
+import { RankingResultType, RankingType } from "./types";
 
 /* export const useFetchRanking = () => {
   const { pesoPesadoPage } = useRankingContext();
@@ -16,17 +16,48 @@ import { RankingResultType } from "./types";
         .then((result) => result.data)
   );
 }; */
+const content: RankingType[] = [
+  {
+    email: "pablo@teste.com",
+    nota: 100,
+    dataEnvio: "10/09/2023",
+    categoria: "PESO_PESADO",
+    linguagem: "JAVA",
+  },
+  {
+    email: "joao@teste.com",
+    nota: 80,
+    dataEnvio: "10/09/2023",
+    categoria: "PESO_PESADO",
+    linguagem: "Node",
+  },
+  {
+    email: "julio@teste.com",
+    nota: 70,
+    dataEnvio: "10/09/2023",
+    categoria: "PESO_PESADO",
+    linguagem: "Meme",
+  },
+];
+
+const result: RankingResultType = {
+  content,
+  total: {
+    elements: 10,
+    pages: 4,
+  },
+  page: {
+    index: 1,
+    last: false,
+    size: 4,
+  },
+};
 
 export const useFetchPesoPesadoRanking = () => {
   const { pesoPesadoPage } = useRankingContext();
-  const { api } = useApi();
+  // const { api } = useApi();
   return useQuery<RankingResultType>(
-    ['peso-pesado-ranking', pesoPesadoPage],
-    () =>
-      api
-        .get('/ranking/PESO_PESADO', {
-          params: { page: pesoPesadoPage - 1, size: 4 },
-        })
-        .then((result) => result.data)
+    ["peso-pesado-ranking", pesoPesadoPage],
+    () => Promise.resolve().then(() => result)
   );
 };
