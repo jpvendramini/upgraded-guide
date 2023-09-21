10452b8cf2cd95bdef7bc97ccadc4034cdfe0822
pablowinck <pablowinck123@gmail.com>
2023-09-21 11:14:43 -0300

    fix #11, #12: filter and sort data of submissions

--- src/components/molecules/EstatisticasCard/layout.tsx
+++ src/components/molecules/EstatisticasCard/layout.tsx
@@ -2,13 +2,9 @@ import "@styles/global.css";
 import { Suspense } from "react";
 
 export default function PodiumLayout({ children }: any) {
-    return (
-        <Suspense
-            fallback={
-                <div className='text-white'>Carregando...</div>
-            }
-        >
-            {children}
-        </Suspense>
-    );
+  return (
+    <Suspense fallback={<div className="text-white">Carregando...</div>}>
+      {children}
+    </Suspense>
+  );
 }
