367cd577bcbba41af44dc387f68a9d00339a4b37
pablowinck <pablowinck123@gmail.com>
2023-09-21 15:12:00 -0300

    fix #21: estatísticas

--- src/components/atoms/LineChart/page.tsx
+++ src/components/atoms/LineChart/page.tsx
@@ -6,10 +6,8 @@ import {
   LineElement,
   LinearScale,
   PointElement,
-  ScaleChartOptions,
   Tooltip,
 } from "chart.js";
-import { useMemo } from "react";
 import { Line } from "react-chartjs-2";
 
 ChartJS.register(
@@ -18,7 +16,7 @@ ChartJS.register(
   PointElement,
   LineElement,
   Tooltip,
-  Filler
+  Filler,
 );
 
 type Props = {
@@ -37,22 +35,19 @@ const options = {
 };
 
 const LineChart = ({ linePoints, dates }: Props) => {
-  const lineData = useMemo(() => {
-    const data = {
-      labels: dates || linePoints.map(() => ""),
-      datasets: [
-        {
-          fill: true,
-          label: "",
-          data: linePoints,
-          borderColor: "#FF7A00",
-          backgroundColor: "#FF7A004D",
-        },
-      ],
-    };
-    return data;
-  }, []);
-  return <Line options={options} data={lineData} />
+  const lineData = {
+    labels: dates || linePoints.map(() => ""),
+    datasets: [
+      {
+        fill: true,
+        label: "",
+        data: linePoints,
+        borderColor: "#FF7A00",
+        backgroundColor: "#FF7A004D",
+      },
+    ],
+  };
+  return <Line options={options} data={lineData} />;
 };
 
 export default LineChart;
