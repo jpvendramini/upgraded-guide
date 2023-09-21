238c852c98f3ea3ed6eab9b9a70829c51071ced6
João Pedro Vendramini <joao.vendramini@muralis.com.br>
2023-09-21 16:42:56 -0300

    feat: contributors page

---
+++ src/app/contributors/contributors.ts
@@ -0,0 +1,67 @@
+const PROFILE_IMAGE_PATH = "/images/contributors/";
+
+export const contributors = [
+  {
+    name: "Pablo Winck Winter",
+    photo: `${PROFILE_IMAGE_PATH}pablo.png`,
+    role: "Organização • Arquitetura • Backend developer • Techtalker",
+    github: "",
+    linkedin: "https://www.linkedin.com/in/pablowinck/",
+  },
+  {
+    name: "Davisson Medeiros",
+    photo: `${PROFILE_IMAGE_PATH}davisson.png`,
+    role: "Organização • Techtalker",
+    github: "",
+    linkedin: "https://br.linkedin.com/in/davisson-medeiros-49674b8b",
+  },
+  {
+    name: "João Pedro Vendramini",
+    photo: `${PROFILE_IMAGE_PATH}joao.jpg`,
+    role: "Frontend Developer • UI/UX",
+    github: "",
+    linkedin: "https://www.linkedin.com/in/joao-pedro-vendramini/",
+  },
+  {
+    name: "Júlio César Alves",
+    photo: `${PROFILE_IMAGE_PATH}julio.png`,
+    role: "Frontend Developer • UI/UX",
+    github: "",
+    linkedin: "https://www.linkedin.com/in/julio-cesar-alves-732b96174",
+  },
+  {
+    name: "Rafael Kenji Takahashi",
+    photo: `${PROFILE_IMAGE_PATH}rafael.png`,
+    role: "Backend developer • QA",
+    github: "",
+    linkedin: "https://www.linkedin.com/in/rafael-takahashi-8567b6196",
+  },
+  {
+    name: "André Oshiro Nishimoto",
+    photo: `${PROFILE_IMAGE_PATH}andre.png`,
+    role: "Backend developer",
+    github: "",
+    linkedin: "https://www.linkedin.com/in/andré-o-nishimoto-3b9aa8169/",
+  },
+  {
+    name: "Everton Silva",
+    photo: `${PROFILE_IMAGE_PATH}everton.png`,
+    role: "Devops • Techtalker",
+    github: "",
+    linkedin: "https://www.linkedin.com/in/everton-silva-9b191553/",
+  },
+  {
+    name: "Alison de Abrantes Duarte",
+    photo: `${PROFILE_IMAGE_PATH}alison.jpg`,
+    role: "Devops",
+    github: "",
+    linkedin: "https://www.linkedin.com/in/alison-duarte/",
+  },
+  {
+    name: "Giovanni da Cunha Massaro",
+    photo: `${PROFILE_IMAGE_PATH}giovanni.png`,
+    role: "Techtalker",
+    github: "",
+    linkedin: "https://www.linkedin.com/in/giovanni-massaro-640a59173/",
+  },
+];
--- src/app/contributors/page.tsx
+++ src/app/contributors/page.tsx
@@ -1,82 +1,53 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { contributors } from "./contributors";
 
 const Contributors = () => {
-  const contributors = [
-    {
-      name: "Pablo Winck Winter",
-      photo: "",
-      role: "Arquitetura • Backend developer • Organização • Techtalker",
-      github: "",
-      linkedin: "",
-    },
-    {
-      name: "João Pedro Vendramini",
-      photo: "",
-      role: "Frontend Developer • UI/UX",
-      github: "",
-      linkedin: "",
-    },
-    {
-      name: "Júlio César Alves",
-      photo: "",
-      role: "Frontend Developer • UI/UX",
-      github: "",
-      linkedin: "",
-    },
-    {
-      name: "Rafael Kenji Takahashi",
-      photo: "",
-      role: "Backend developer • QA",
-      github: "",
-      linkedin: "",
-    },
-    {
-      name: "André Oshiro Nishimoto",
-      photo: "",
-      role: "Backend developer",
-      github: "",
-      linkedin: "",
-    },
-    {
-      name: "Everton Silva",
-      photo: "",
-      role: "Devops • Techtalker",
-      github: "",
-      linkedin: "",
-    },
-    {
-      name: "Alison de Abrantes Duarte",
-      photo: "",
-      role: "Devops",
-      github: "",
-      linkedin: "",
-    },
-    {
-      name: "Davisson Medeiros",
-      photo: "",
-      role: "Techtalker",
-      github: "",
-      linkedin: "",
-    },
-    {
-      name: "Giovanni da Cunha Massaro",
-      photo: "",
-      role: "Techtalker",
-      github: "",
-      linkedin: "",
-    },
-  ];
-
   return (
     <div className="grid grid-cols-2 w-full font-sans gap-8">
+      <Link
+        href="/dashboard"
+        className="fixed top-10 left-10 text-white flex gap-2 font-bold justify-center items-center"
+      >
+        <Image
+          alt="profile image"
+          src="icons/chevron-left.svg"
+          width={8}
+          height={8}
+        />
+        Voltar
+      </Link>
       {contributors.map((contributor) => (
-        <div className="flex gap-4 items-center">
-          <div className="rounded-full bg-[#D9D9D9] w-[3rem] h-[3rem] flex items-center justify-center font-bold">
-            {contributor.name.substring(0, 2)}
-          </div>
-          <div className="text-white max-w-[12rem]">
-            <h2 className="font-bold">{contributor.name}</h2>
-            <p className="text-sm">{contributor.role}</p>
+        <div className="flex gap-4 items-center grayscale hover:grayscale-0 transition-all duration-300">
+          {contributor.photo && (
+            <div
+              className="w-[3rem] h-[3rem] rounded-full overflow-hidden relative"
+              style={{
+                objectFit: "cover",
+              }}
+            >
+              <Image
+                alt=""
+                src={contributor.photo}
+                fill
+                className="object-cover"
+              />
+            </div>
+          )}
+          {!contributor.photo && (
+            <div className="rounded-full bg-[#D9D9D9] w-[3rem] h-[3rem] flex items-center justify-center font-bold">
+              {contributor.name.substring(0, 2)}
+            </div>
+          )}
+          <div className="flex flex-col text-white max-w-[12rem] gap-1">
+            <Link
+              href={contributor.linkedin || contributor.github}
+              target="_blank"
+            >
+              <h2 className="font-bold">{contributor.name}</h2>
+            </Link>
+            <p className="text-sm capitalize">{contributor.role}</p>
           </div>
         </div>
       ))}
